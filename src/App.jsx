f10e4d4e3c0b896fd97b292f63d4597c213e772e
ssenyondo ambrose <ssenyondoambrose@gmail.com>
2023-08-09 11:57:09 +0300

    added newfiles

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App(){
+  return (
+    <>
+         <div>
+      <h1 className='head'>Daily Quotes</h1>
+    </div>
+    <div>
+      <p className='qoute'>"the life we love is the life that's been lived"</p>
+      <p className='qoute'>"A great man is always willing to be little"by Ralph waledo</p>
+      <p className='qoute'>"After crosses and losses men grow humbler and wiser"by Benjamin Franklin</p>
+      <p className='qoute'>"Humillity makes great men twice honorable" by Benjamin Franklin</p>
+      <p className='qoute'>"If you hear how wonderful you are often enough, you begin to believe it,
+        <br/> no matter how you try to resist it."by Ben Carson</p>
+        <p className='qoute'>"The less people speak of their greatness, the more we think of it"by Lord Bacon</p>
+
+    </div>
+    </>
+  );
+}
+
+export default App;
